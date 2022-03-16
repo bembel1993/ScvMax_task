1cd0908e63f3336ba0559157fcb98cbb3ec3f083
bembel1993 <78043683+bembel1993@users.noreply.github.com>
2022-03-16 10:22:15 +0300

    Add files via upload

--- table_3/CSVfile.java
+++ table_3/CSVfile.java
@@ -3,19 +3,22 @@ package table_3;
 import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
+import table_1.Employee;
 
-import java.io.IOException;
-import java.io.StringWriter;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CSVfile {
-
+    public static File writeTable = new File("table3.txt");
+    public static File writesolution= new File("solution3.txt");
+    public static char[] buf;
     public static void main(String[] args) throws IOException {
         List<City> citys = addData();
         StringWriter writer = new StringWriter();
 
-        System.out.println("///////////////////TABLE ONE//////////////////////////////////");
+        System.out.println("///////////////////TABLE THREE//////////////////////////////////");
         ICsvBeanWriter csvBeanWriter = new CsvBeanWriter(writer, CsvPreference.STANDARD_PREFERENCE);
         String[] header = new String[]{"city", "temp2", "temp"};
         //create Title
@@ -31,7 +34,7 @@ public class CSVfile {
         table_3.CSVfile.minValueInColumnMass();
     }
     ///////////////////////////////////////////////////////////////////
-    private static List<City> addData() {
+    private static List<City> addData() throws IOException {
         List<City> cities = new ArrayList<>();
         City city1 = new City("Minsk", 6, -9);
         City city2 = new City("Brest", 4, -7);
@@ -40,11 +43,14 @@ public class CSVfile {
         cities.add(city1);
         cities.add(city2);
         cities.add(city3);
-
+        //WRITE IN FILE
+        writeAddWordInFile(cities);
+        //READE IN FILE
+        showWriteAddWordInFile();
         return cities;
     }
 
-    public static int minValueInColumnMass(){
+    public static int minValueInColumnMass() throws IOException {
         int number = 0;
         int max = -1000;
         for (City str : addData()){
@@ -54,7 +60,48 @@ public class CSVfile {
             }
         }
         System.out.println("Method 1. Max Value in column temp: " + max);
+        FileWriter fileWriter = new FileWriter(writesolution, true);
+        BufferedWriter writeWordInFile = new BufferedWriter(fileWriter);
+        System.out.println("Date result out solution");
+        System.out.println("      -       ");
+        writeWordInFile.write(String.valueOf(max)+"\n");
+        writeWordInFile.flush();
+        showWriteAddWordInFileSolut();
         return max;
     }
+    public static void writeAddWordInFile(List<City> cities) throws IOException {
+        FileWriter fileWriter = new FileWriter(writeTable, true);
+        BufferedWriter writeWordInFile = new BufferedWriter(fileWriter);
+        System.out.println("Date is Write");
+        System.out.println("      -       ");
+        writeWordInFile.write(String.valueOf(cities));
+        writeWordInFile.flush();
+    }
 
+    public static void showWriteAddWordInFile() throws IOException {
+        FileReader fileReadAddToVocabulary = new FileReader(writeTable);
+        buf = new char[10000];
+        int c; //- by character reading
+        try {
+            c = fileReadAddToVocabulary.read(buf);
+            buf = Arrays.copyOf(buf, c);
+            System.out.print(buf);
+            System.out.print("\n");
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
+    }
+    public static void showWriteAddWordInFileSolut() throws IOException {
+        FileReader fileReadAddToVocabulary = new FileReader(writesolution);
+        buf = new char[10000];
+        int c; //- by character reading
+        try {
+            c = fileReadAddToVocabulary.read(buf);
+            buf = Arrays.copyOf(buf, c);
+            System.out.print(buf);
+            System.out.print("\n");
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
+    }
 }
